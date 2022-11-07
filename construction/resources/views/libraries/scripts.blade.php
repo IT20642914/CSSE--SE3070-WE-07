e2b87b84ac8904dab0d236449fac61e00e280e7a
Sachini Sathsarani Wijesinghe <it20641092@my.sliit.lk>
2022-11-07 17:00:09 +0530

    navbar

---
+++ construction/resources/views/libraries/scripts.blade.php
@@ -0,0 +1,9 @@
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+</script>
---
+++ construction/resources/views/pages/item/additem.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('content')
+
+@endsection
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+</script>
